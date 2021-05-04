2ae7c1ae99a38f54c701110d7ddd28fd63321e98
mtsenkov <matthew.tsenkov@gmail.com>
2021-05-04 17:23:31 -0400

    Blank skeleton of the project

---
+++ Source/PrisonEscape/PrisonEscape.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PrisonEscape.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PrisonEscape, "PrisonEscape" );
---
+++ Source/PrisonEscape/PrisonEscapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PrisonEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRISONESCAPE_API APrisonEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
